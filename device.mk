125021991eb53df493d8e76131cab2a9edfe7c73
Sato-goj <kaloresanket@gmail.com>
2025-06-12 23:39:03 +0530

    pstar: Rewire to fix build errors and bootloop

--- device.mk
+++ device.mk
@@ -7,32 +7,32 @@
 # Inherit from sm8250-common
 $(call inherit-product, device/motorola/sm8250-common/common.mk)
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Overlays
+PRODUCT_PACKAGES += \
+    FrameworksResPstar \
+    SystemUIResPstar \
+    SettingsProviderResPstar
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
+# Display
+PRODUCT_VENDOR_PROPERTIES += \
+    debug.graphics.game_default_frame_rate.disabled=1
+
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.device.default_name=edge 20 pro
 
-# Call recording
-PRODUCT_PACKAGES += \
-    com.google.android.apps.dialer.call_recording_audio.features.xml
-
 # Camera
 PRODUCT_PACKAGES += \
     libgui_shim_vendor
-
-# Dex preopt
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    Launcher3QuickStep \
-    SettingsGoogle \
-    SystemUIGoogle
-
-# Display
-PRODUCT_VENDOR_PROPERTIES += \
-    debug.graphics.game_default_frame_rate.disabled=1
 
 # Init
 PRODUCT_PACKAGES += \
@@ -42,31 +42,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml
 
+# Dex preoptAdd commentMore actions
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Launcher3QuickStep \
+    SettingsGoogle \
+    SystemUIGoogle
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service.st \
     android.hardware.secure_element@1.2-service.st \
     nfc_nci.st21nfc.default
-
-# Overlays
-PRODUCT_PACKAGES += \
-    FrameworksResPstar \
-    SystemUIResPstar \
-    SettingsProviderResPstar
-
-# Overlays _Lineage
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml \
-    frameworks/native/data/etc/android.software.picture_in_picture.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.picture_in_picture.xml
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/pstar/pstar-vendor.mk)
--- lineage_pstar.mk
+++ lineage_pstar.mk
@@ -12,19 +12,24 @@ PRODUCT_NAME := lineage_pstar
 PRODUCT_DEVICE := pstar
 PRODUCT_MODEL := motorola edge 20 pro
 
-# Environment Flags
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_STOCK_ARCORE := true
+# risingOS-Flags
+
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
 TARGET_ENABLE_BLUR := true
-TARGET_USE_GOOGLE_TELEPHONY := true
+WITH_GMS := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+TARGET_INCLUDE_GOOGLE_DIALER := true
+TARGET_USES_MINI_GAPPS := true
+
+# rising Maintainer
+RISING_MAINTAINER := Satoru Gojo
+
 
 # Build info
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="pstar_retail-user 13 T1RAS33.55-15-16-5 d9def9-6b8134 release-keys" \
     BuildFingerprint=motorola/pstar_retail/pstar:13/T1RAS33.55-15-16-5/d9def9-6b8134:user/release-keys \
     DeviceProduct=pstar_retail \
-    SystemName=pstar_retail
+    SystemName=pstar_retail \
+    RisingMaintainer="Satoru Gojo" \
+    RisingChipset="SD 870 5G"
